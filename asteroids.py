a5788b81c652034bca55bd1ee391b3e8769bf6b2
beliverable <bogdan.secara@gmail.com>
2025-04-14 19:03:29 +1200

    Eleventh major change

--- asteroids.py
+++ asteroids.py
@@ -3,19 +3,10 @@ from circleshape import CircleShape
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
-        self.x = x
-        self.y = y
-        self.radius = radius
+        super().__init__(x, y, radius)
     
-    def draw(self,surface, color, position, radius, width = 2):
-        self.surface = surface
-        self.position = position
-        self.radius = radius
-        self.width = width
-        self.color = "white"
-        pygame.draw.circle(self.surface, 
-                           self.color, 
-                           self.position, 
-                           self.radius, 
-                           self.width
-                           )
+    def draw(self,screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius, 2)
+ 
+    def update(self, dt):
+        self.position = self.velocity * dt
